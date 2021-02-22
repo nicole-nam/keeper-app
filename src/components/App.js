ef58be465f9612e477f9574f61ed008392117c77
Eunjung Nam <eunjungnam@eunjungs-air.lan>
2021-02-22 16:28:39 -0500

    add notes title and content to App.js

---
+++ src/components/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import notes from "../notes";
+
+function App() {
+  console.log(notes);
+
+  return (
+    <div>
+      <Header />
+      {notes.map((note) => {
+        return (
+          <Note
+            key={note.Item}
+            title={note.title}
+            content={note.content}
+          />
+        );
+      })}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
